c07b1c63d1843a6180ff71f1dfd46230ba64a30a
yoojeho <jeho328@naver.com>
2018-04-14 17:30:07 +0900

    Update the submit function of contact
    If submitted the content is incorrect or empty, send a alert message.
    If submit is successful, save contents to DB

--- route/index.js
+++ route/index.js
@@ -1,28 +1,54 @@
-module.exports = function () {
-	var route = require('express').Router();
-	var conn = require('../config/database')();
+const route = require('express').Router();
+const conn = require('../config/database')();
 
-	route.get('/', function (req, res) {
-		var user = req.user;
+module.exports = () => {
+	route.get('/', (req, res) => {
+		const { user } = req;
+
 		if (user) {
-			var authId = user.authId;
-			var email = user.email;
-			var sql = 'SELECT * FROM users WHERE authId=?';
-			conn.query(sql, [authId], function (err, results) {
-				var profilePicture = results[0].profilePicture;
-				var pP = 'images/userprofile/' + email + '/profilePicture/' + profilePicture;
-				console.log('pP: '+pP);
+			const { authId } = user;
+			const { email } = user;
+			const sql = 'SELECT * FROM users WHERE authId=?';
+			conn.query(sql, [authId], (err, results) => {
+				const { profilePicture } = results[0];
+				const pP = 'images/userprofile/' + email + '/profilePicture/' + profilePicture;
 				res.render('about_me', {
 					user: user,
-					pP: pP
+					pP: pP,
 				});
-			})
+			});
 		} else {
 			res.render('about_me');
 		}
+	});
 
-		
+	// contact 제출시
+	route.post('/contact_submit', (req, res) => {
+		const { email } = req.body;
+		const { title } = req.body;
+		const { message } = req.body;
+		console.log(message);
+		if (email === '') return res.send('email_null');
+		if (email === 'invalid') return res.send('email_invalid');
+		if (title === '') return res.send('title_null');
+		if (message === '') return res.send('message_null');
+		const d = new Date();
+		const time = `${d.getFullYear()}년${parseInt(d.getMonth() + 1, 10)}월${d.getDate()}일 ${d.getHours()}:${d.getMinutes()}`;
+		const contact_form = {
+			email,
+			title,
+			message,
+			time,
+		};
+		const sql = 'INSERT INTO contacts SET ?';
+		return conn.query(sql, contact_form, (err) => {
+			if (err) {
+				console.log(err);
+				return res.send('err');
+			}
+			return res.send('success');
+		});
 	});
 
 	return route;
-}
+};
